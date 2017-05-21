e03d64a1822f9fc05d0f3b828b9185602526b912
Moderbord <pontande@gmail.com>
2017-05-21 16:32:52 +0200

    Finally fixed the inconsistent deathcollider from bombs *crossing thumbs*

--- Assets/Scripts/WeaponScripts/Bombscript.cs
+++ Assets/Scripts/WeaponScripts/Bombscript.cs
@@ -8,8 +8,9 @@ public class Bombscript : MonoBehaviour {
     [Space(10), Tooltip("Setting for maximum fuse time. Value will be randomed between 2f and this value"), Range(2f, 5f)]
     public float fuseTime = 3f;
 
-    [Space(10), Tooltip("The explosion effect that will be instatiated")]
+    [Space(10), Tooltip("The explosion effect and deathArea that will be instatiated")]
     public GameObject explosion;
+    public GameObject deathCircle;
 
     Rigidbody2D body;
 
@@ -26,24 +27,18 @@ public class Bombscript : MonoBehaviour {
 
     IEnumerator DelayedExplosion()
     {
-        CircleCollider2D collider = GetComponent<CircleCollider2D>();
-
         yield return new WaitForSeconds(Random.Range(2f, fuseTime));
- 
-        collider.radius = 2.5f;
-        transform.tag = "DeathCollider";
-
-        // The short delay before bomb explodes
-        yield return new WaitForSeconds(1/5);
 
         SpriteRenderer render = GetComponentInChildren<SpriteRenderer>();
         render.enabled = false;
 
         // Instatiates an explosion and destroys it after some time
         GameObject effect = Instantiate(explosion, body.position, Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;
+        GameObject death = Instantiate(deathCircle, body.position, Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;
 
         Destroy(effect, 2f);
-        Destroy(gameObject, 1/5f);
+        Destroy(death, 1/5f);
+        Destroy(gameObject);
     }
 
 
